bf3b80ff9f59705c107e8d102ee706cb2c4bc47e
Milosz B <miloszbargiel@gmail.com>
2022-08-12 16:43:47 +0200

    Add todoData into the server 'database' and setup sockets

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import path from 'path';
+import { Server } from 'socket.io';
+
+interface todoData {
+  id: string;
+  task: string;
+}
 
 const buildDir = path.join(process.cwd() + '/build');
 const app = express();
 const port = 8000;
+
+const todos: todoData[] = [
+  {id: '1', task: 'Go out with the dog.'},
+  {id: '2', task: 'Study for at least 2 hours.'},
+  {id: '3', task: 'Clean up the kitchen.'}
+];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +25,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(buildDir, 'index.html'));
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`running on port: ${port}.`);
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'DELETE'],
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log(todos)
+  socket.emit('getData', todos);
+  console.log(socket.id)
 });
