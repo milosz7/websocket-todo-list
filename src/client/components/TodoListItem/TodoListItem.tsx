dc95540563e1df5672d85ff8e670da1f3dbfa3dc
Milosz B <miloszbargiel@gmail.com>
2022-08-12 18:03:01 +0200

    Add styles for TodoListItem

--- src/client/components/TodoListItem/TodoListItem.tsx
+++ src/client/components/TodoListItem/TodoListItem.tsx
@@ -1,14 +1,15 @@
 import { TodosContext } from "../../context/TodosContext";
 import { useContext } from "react";
 import { todoContext } from "../../types/types";
+import styles from './TodoListItem.module.css';
 
 const TodoListItem = ({ id, task }: {id: string, task: string}) => {
   const { removeTodo } = useContext(TodosContext) as todoContext;
   
   return (
-    <li>
+    <li className={styles.item}>
       <p>{task}</p>
-      <button onClick={() => removeTodo(id)} type="button">Mark as finished</button>
+      <button className={styles.button} onClick={() => removeTodo(id)} type="button">done</button>
     </li>
   )
 };
