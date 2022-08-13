0fefa6fa1d952eef0721d4496a13cae103914a84
Milosz B <miloszbargiel@gmail.com>
2022-08-13 18:30:40 +0200

    Add styles for TodosList child component layout

--- src/client/components/TodosList/TodosList.tsx
+++ src/client/components/TodosList/TodosList.tsx
@@ -3,6 +3,7 @@ import { TodosContext } from "../../context/TodosContext";
 import { todoContext } from "../../types/types";
 import TodoListItem from "../TodoListItem/TodoListItem";
 import styles from './TodosList.module.css';
+import AddTodoForm from "../AddTodoForm/AddTodoForm";
 
 const TodosList = () => {
   const { todos } = useContext(TodosContext) as todoContext;
@@ -10,6 +11,9 @@ const TodosList = () => {
   return (
     <ul className={styles.list}>
       {todos.map((todoData, idx) => <TodoListItem key={idx} {...todoData} />)}
+      <div className={styles.layout}>
+      <AddTodoForm />
+      </div>
     </ul>
   )
 };
