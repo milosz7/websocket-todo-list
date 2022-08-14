88dace58089d1c8edacacebef7855bf1e98d9385
Milosz B <miloszbargiel@gmail.com>
2022-08-14 17:31:04 +0200

    Add spinner component

---
+++ src/client/components/Spinner/Spinner.tsx
@@ -0,0 +1,33 @@
+const Spinner = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="200px"
+      height="200px"
+      viewBox="0 0 100 100"
+      preserveAspectRatio="xMidYMid"
+    >
+      <circle
+        cx="50"
+        cy="50"
+        fill="none"
+        stroke="#fff"
+        strokeWidth="6"
+        r="15"
+        strokeDasharray="70.68583470577033 25.561944901923447"
+      >
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          repeatCount="indefinite"
+          dur="1s"
+          values="0 50 50;360 50 50"
+          keyTimes="0;1"
+        ></animateTransform>
+      </circle>
+    </svg>
+  );
+};
+
+export default Spinner;
